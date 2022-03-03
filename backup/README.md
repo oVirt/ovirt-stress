07ea60f467b1ce2f3c0931606d2a6f8c2b4f4f16
Nir Soffer <nsoffer@redhat.com>
2022-03-03 16:55:10 +0200

    Add backup stress test
    Create a configuration and ansible setup for backup vm template.  We
    start with a minimal vm using single OS disk. More disks will be added
    later, or maybe we will use multiple templates.
    
    The new test is mostly a copy of the delete snapshot test. When the
    test is finished, we can start thinking how to reuse test code better.
    
    The imagetransfer module was borrowed form the python sdk examples
    helpers with minimal changes. The backup is based on the backup_vm.py
    example.

---
+++ backup/README.md
@@ -0,0 +1,88 @@
+# Stress test for incremental backup
+
+## Overview
+
+This test performs concurrent full and incremental backup.
+
+## Physical environment
+
+The test can run with one host, but it is recommended to test on at
+least 2 hosts so we can test backup on the SPM host and on regular host.
+
+## Creating a test configuration
+
+Please create a file named "conf.yml" by copying "conf.yml.example" and
+modifying the variables.
+
+### Engine connection
+
+    engine_fqdn: "engine-fqdn"
+    engine_username: "admin@internal"
+    engine_password: "engine-password"
+    engine_cafile: "/etc/pki/vdsm/certs/cacert.pem"
+
+The default ``engine_cafile`` is correct when you run this test on one
+of the hosts.
+
+### Storage configuration
+
+This is the template used to create the test VM template. This template
+must have a public key of the host running the test. If needed, create a
+public key with empty passphrase the host using ssh-keygen. This public
+key is used by the test to connect to the test VM, and write data to the
+disks.
+
+    template_base: "template-base"
+
+The storage domain name for creating the template and the test VMs.
+
+    storage_domain: "storage-domain"
+
+### Cluster configuration
+
+The cluster name for creating the template test VMs. This cluster must
+contain the selected host.
+
+    cluster_name: "cluster-name"
+
+### Controlling test size and duration
+
+By default, the test creates 8 test VMs. On a smaller environment,
+testing with lower number of VMs may be needed.
+
+    vms_count: 8
+
+Specify the number of test iterations for every VM. The default is good
+for verifying that everything works. After that you can configure this
+base on the time you want to run the test. On our test system one
+iteration takes about 5 minutes.
+
+    iterations: 1
+
+### Troubleshooting
+
+If something goes wrong, you can enable very verbose logs to debug the
+test.
+
+    debug: true
+
+## Creating a test template
+
+Before running the test, you need to create the test template once.
+
+Please run:
+
+    ansible-playbook setup.yml
+
+This creates a template named "backup-template". The test VMs
+will be created from this template.
+
+## Running the test
+
+To run the tests please run:
+
+    python3 test.py
+
+To save the test log use:
+
+    python3 test.py 2>test.log
---
+++ backup/backup.py
@@ -0,0 +1,230 @@
+"""
+oVirt backup helpers.
+"""
+
+import glob
+import json
+import logging
+import os
+import ssl
+import time
+
+from contextlib import closing
+from http import client as http_client
+from urllib.parse import urlparse
+
+from ovirt_imageio import client
+
+import ovirtsdk4 as sdk
+import ovirtsdk4.types as types
+
+import imagetransfer
+
+log = logging.getLogger("backup")
+
+
+def start_backup(connection, vm, from_checkpoint=None, backup_id=None,
+                 description=None):
+    log.info("Starting backup for vm %r from checkpoint %r",
+             vm.id, from_checkpoint)
+
+    start = time.monotonic()
+
+    vm_service = _get_vm_service(connection, vm)
+
+    disk_attachments = vm_service.disk_attachments_service().list()
+    disks = [connection.follow_link(a.disk) for a in disk_attachments]
+
+    backups_service = vm_service.backups_service()
+    backup = backups_service.add(
+        types.Backup(
+            id=backup_id,
+            disks=disks,
+            from_checkpoint_id=from_checkpoint,
+            description=description,
+        ),
+        # Hack until we have a proper API model change.
+        query={'with_snapshot': 'true'},
+    )
+
+    log.info("Waiting until backup %r is ready", backup.id)
+
+    backup_service = backups_service.backup_service(backup.id)
+
+    while backup.phase != types.BackupPhase.READY:
+        time.sleep(5)
+        backup = _get_backup(backup_service, backup.id)
+
+    log.info("Backup %r started in %d seconds",
+             backup.id, time.monotonic() - start)
+
+    return backup
+
+
+def stop_backup(connection, backup):
+    log.info("Stopping backup %r", backup.id)
+
+    start = time.monotonic()
+
+    backup_service = _get_backup_service(connection, backup)
+
+    backup_service.finalize()
+
+    log.info("Waiting until backup %r stops", backup.id)
+
+    while backup.phase != types.BackupPhase.SUCCEEDED:
+        time.sleep(5)
+        backup = _get_backup(backup_service, backup.id)
+
+    log.info("Backup %r stopped in %d seconds",
+             backup.id, time.monotonic() - start)
+
+
+def download_backup(connection, backup, backup_dir, ca_file=None,
+                    secure=False, verify=False):
+    log.info("Downloading backup %r disks", backup.id)
+
+    start = time.monotonic()
+
+    backup_service = _get_backup_service(connection, backup)
+    backup_disks = backup_service.disks_service().list()
+
+    for disk in backup_disks:
+        incremental = disk.backup_mode == types.DiskBackupMode.INCREMENTAL
+        file_name = "{}.{}.{}.qcow2".format(
+            backup.to_checkpoint_id, disk.id, disk.backup_mode)
+        filename = os.path.join(backup_dir, file_name)
+
+        if incremental:
+            backing_file = _find_backing_file(
+                backup_dir, backup.from_checkpoint_id, disk.id)
+        else:
+            backing_file = None
+
+        _download_disk(
+            connection, backup, disk, filename,
+            incremental=incremental,
+            backing_file=backing_file,
+            ca_file=ca_file,
+            secure=secure,
+            verify=verify)
+
+    log.info("Backup %r downloaded in %d seconds",
+             backup.id, time.monotonic() - start)
+
+    return filename
+
+
+def _download_disk(connection, backup, disk, filename, incremental=False,
+                   backing_file=None, ca_file=None, secure=False,
+                   verify=False):
+    log.info("Downloading %s backup for disk %r to file %r using backing_file %r",
+             "incremental" if incremental else "full", disk.id, filename,
+             backing_file)
+
+    start = time.monotonic()
+
+    transfer = imagetransfer.create_transfer(
+        connection,
+        disk,
+        types.ImageTransferDirection.DOWNLOAD,
+        backup=types.Backup(id=backup.id),
+    )
+    try:
+        client.download(
+            transfer.transfer_url,
+            filename,
+            ca_file,
+            incremental=incremental,
+            secure=secure,
+            backing_file=backing_file,
+            backing_format="qcow2",
+        )
+
+        if verify:
+            expected = _disk_checksum(transfer, ca_file)
+            actual = _backup_checksum(filename)
+            if expected != actual:
+                raise RuntimeError(
+                    f"Checksum mismatch: expected {expected} got {actual}")
+    finally:
+        imagetransfer.finalize_transfer(connection, transfer, disk)
+
+    log.info("Download completed in %d seconds", time.monotonic() - start)
+
+
+def _disk_checksum(transfer, ca_file):
+    log.info("Computing disk checksum")
+
+    start = time.monotonic()
+
+    url = urlparse(transfer.transfer_url)
+    con = http_client.HTTPSConnection(
+        url.netloc,
+        context=ssl.create_default_context(cafile=ca_file))
+    with closing(con):
+        con.request("GET", url.path + "/checksum")
+        res = con.getresponse()
+        if res.status != http_client.OK:
+            error = res.read().decode("utf-8", "replace")
+            raise RuntimeError(f"Error computing checksum: {error}")
+
+        result = json.loads(res.read())
+
+    log.info("Disk checksum computed in %d seconds", time.monotonic() - start)
+
+    return result
+
+
+def _backup_checksum(filename):
+    log.info("Computing backup checksum")
+    start = time.monotonic()
+    result = client.checksum(filename)
+    log.info("Backup checksum computed in %d seconds",
+             time.monotonic() - start)
+    return result
+
+
+# General helpers
+
+def _get_vm_service(connection, vm):
+    system_service = connection.system_service()
+    return system_service.vms_service().vm_service(vm.id)
+
+
+def _get_backup_service(connection, backup):
+    system_service = connection.system_service()
+    vms_service = system_service.vms_service()
+    backups_service = vms_service.vm_service(backup.vm.id).backups_service()
+    return backups_service.backup_service(backup.id)
+
+
+def _get_backup(backup_service, backup_id):
+    """
+    Get backup, raising if backup does not exist or failed, so code monitoring
+    backup phase does not have to repeat these checks.
+    """
+    try:
+        backup = backup_service.get()
+    except sdk.NotFoundError:
+        raise RuntimeError(f"Backup {backup_id} does not exist")
+
+    if backup.phase == types.BackupPhase.FAILED:
+        raise RuntimeError(f"Backup {backup_id} has failed")
+
+    return backup
+
+
+def _find_backing_file(backup_dir, checkpoint_id, disk_id):
+    """
+    Return the name of the backing file for checkpoint, or None if the file was
+    not found.
+    """
+    pattern = os.path.join(backup_dir, f"{checkpoint_id}.{disk_id}.*.qcow2")
+    matches = glob.glob(pattern)
+    if not matches:
+        return None
+
+    # The backing file can be an absolute path or a relative path from the
+    # image directory. Using a relative path make is easier to manage.
+    return os.path.relpath(matches[0], backup_dir)

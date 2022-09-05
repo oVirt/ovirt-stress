63e2ee3b95154047f099b794227c1042c43b6b85
Albert Esteve <aesteve@redhat.com>
2022-09-05 14:59:12 +0200

    pylintrc: enable wrong-import-order
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- backup/test.py
+++ backup/test.py
@@ -5,10 +5,9 @@ import subprocess
 import threading
 import time
 import uuid
-import yaml
-
 from collections import Counter
 
+import yaml
 import ovirtsdk4 as sdk
 import ovirtsdk4.types as types
 
